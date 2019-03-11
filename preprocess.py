8f7e618ec53d3f60e0df8682aa71bac2401f36dc
giovanni <giovanni.gabbolini@gmail.com>
2019-03-11 11:36:23 +0100

    fixed merge

--- preprocess.py
+++ preprocess.py
@@ -7,13 +7,7 @@ import preprocess.create_matrices as create
 
 train = data.train_df()
 test = data.test_df()
-<<<<<<< HEAD
 accomodations = data.accomodations_id()
 print('data loaded')
 u, session_ids, dict_acc, hdl = create.urm(train, test, accomodations, local=False, save=False)
-=======
-accomodations = data.accomodations_df()
-u, session_ids, dict_acc, hdl = create.urm(train, test, accomodations, local=False, save=False)
-
->>>>>>> master
 print(u.shape)
