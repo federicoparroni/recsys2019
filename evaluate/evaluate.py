d38e954d2f0a326101bac7151bd5ecfbbdc11959
Unknown <damicoedoardo@virgilio.it>
2019-03-12 12:10:16 +0100

    Evaluate on recommender base completed
    added the evaluate method on the recommender base class, implemented with python the cython version have to be implemented

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import time
 import evaluate_C
+import data
 
 TEST_PATH = "dataset/sample_test.csv"
 test_df = pd.read_csv(TEST_PATH)
@@ -11,18 +12,24 @@ predictions_sample = [('62991f7c78f27', [7818446,51315,2133708,119638,86359,1335
                       ('67c4d45f56146', [6010044,5433188,5156000,4780400,5137162,9928194,7381410,4929038,5521566,10055274,2120542,6473358,3167630,4600214,2681512,1774631,5873826,4046084,4836154,8526468,6395824,7413354,6474410,38,35,54,37,73,37,37,30,71,75,64,26,25,37,40,36,72,33,23,41,56,25,63]),
                       ('e84393cf62d13', [3132957,100226,1954167,9462680,2776177,929533,42819,42475,1346336,1001753,34468,4063872,34478,42467,4635302,9659264,2346866,1001785,34448,129053,1717401,3515296,3166108,3515434,7136232])]
 
-def evaluate(test_df, predictions):
+
+def evaluate(predictions, mode):
     """
-    :param test_df: pandas dataframe composed by user_id,session_id,timestamp,step,clickout,impressions_list
+    :param mode: 'local' or 'small' say which train has been used
     :param predictions: session_id, ordered impressions_list
     :return:
     """
+    assert (mode == 'local' or mode == 'small')
 
-    test = np.array(test_df)
+    handle = data.handle_df(mode)
+    test = np.array(handle)
+
+    # initialize reciprocal rank value
     RR = 0
+
     target_session_count = test.shape[0]
 
-    for i in range(target_session_count):
+    for i in tqdm(range(target_session_count)):
         position = (np.where(np.array(predictions[i][1]) == test[i, 4]))[0][0]
         RR += 1/(position+1)
 
