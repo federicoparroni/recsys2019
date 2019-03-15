ca920eee561269da0384f3317e037abaf8cad5c4
Unknown <damicoedoardo@virgilio.it>
2019-03-15 10:56:53 +0100

    clean_data added
    file to implement some cleaning method for the data

--- recommenders/distance_based_recommender.py
+++ recommenders/distance_based_recommender.py
@@ -7,13 +7,18 @@ for documentation and distance formulas
 import os
 import sys
 sys.path.append(os.getcwd())
-
+import time
 from recommenders.recommender_base import RecommenderBase
 import utils.log as log
 import numpy as np
 import similaripy as sim
 import data
 from tqdm import tqdm
+
+from functools import partial
+import pathos.pools as pp
+import multiprocessing
+
 
 class DistanceBasedRecommender(RecommenderBase):
     """
@@ -40,6 +45,8 @@ class DistanceBasedRecommender(RecommenderBase):
         self.urm_name = urm_name
         self._sim_matrix = None
         self._matrix_mul_order = 'standard' # if you want R•R', or 'inverse' if you want to compute S•R
+        self.R_hat = None
+        self.dict_col = None
 
     def fit(self, matrix, k, distance, shrink=0, threshold=0, implicit=True, alpha=0.5, beta=0.5, l=0.5, c=0.5, urm=None):
         """
@@ -139,24 +146,76 @@ class DistanceBasedRecommender(RecommenderBase):
        	print('recommending batch')
         if not self._has_fit():
             return None
-
         df_handle = data.handle_df(mode=self.mode)
-        dict_row = data.dictionary_row(mode=self.mode)
+        #dict_row = data.dictionary_row(mode=self.mode)
         dict_col = data.dictionary_col(mode=self.mode)
 
         # compute the R^ by multiplying: R•S or S•R
-        R_hat = self.get_r_hat(verbose)
+        self.R_hat = self.get_r_hat(verbose)
+        print("R_hat computed")
         
-        target_rows = data.target_urm_rows(self.mode)
+        #target_rows = data.target_urm_rows(self.mode)
         predictions = []
         for index, row in tqdm(df_handle.iterrows()):
             impr = list(map(int, row['impressions'].split('|')))
-            urm_row = R_hat.getrow(index)
-            l = [[i, urm_row[0, dict_col[i]]] for i in impr]
+            l = [[i, self.R_hat[index, dict_col[i]]] for i in impr]
             l.sort(key=lambda tup: tup[1], reverse=True)
             predictions.append((row['session_id'], [e[0] for e in l]))
 
         return predictions
+
+
+    def multi_thread_recommend_batch(self, verbose=False):
+        print('recommending batch')
+
+        if not self._has_fit():
+            return None
+
+        df_handle = data.handle_df(mode=self.mode)
+        self.dict_col = data.dictionary_col(mode=self.mode)
+
+        # compute the R^ by multiplying: R•S or S•R
+        self.R_hat = self.get_r_hat(verbose)
+        print("R_hat computed")
+
+        predictions = []
+
+        """
+        multiprocessing part
+        """
+        # instance a number of workers equal to the cpu of the machine
+        workers = multiprocessing.cpu_count()
+
+        #define the call to the function _recommend_row passing the fix parameter (in this case R_hat)
+
+        #create a pool with a number of processes equal to the cpu count
+        pool = pp.ProcessPool(workers)
+
+        # add the indexes as the first column of the dataframe
+        df_handle.reset_index(inplace=True)
+
+        # convert the handle from dataframe to numpy array
+        handle_array = df_handle.values
+
+        start = time.time()
+        # start the pool passing to the function with the fixed input the remaining variable parameter (handle_array)
+        predictions = pool.map(self._recommend_row, handle_array)
+        pool.close()
+        pool.join()
+        print("recommendations created in {}:".format(time.time()-start))
+
+
+        return predictions
+
+
+    def _recommend_row(self, row):
+
+        #TODO: HAVE TO BE CHANGED
+        print(row[0])
+        impr = list(map(int, row[6].split('|')))
+        l = [[i, self.R_hat[row[0], self.dict_col[i]]] for i in impr]
+        l.sort(key=lambda tup: tup[1], reverse=True)
+        return row['session_id'], [e[0] for e in l]
 
     def run(self):
         pass
