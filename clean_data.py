f8f3f933d53e0ec6155e126cb2699d003efacb9e
Unknown <damicoedoardo@virgilio.it>
2019-03-18 17:11:06 +0100

    ICM creation + attributes frequency visualization

--- clean_data.py
+++ clean_data.py
@@ -3,6 +3,11 @@ import data
 import numpy as np
 from tqdm import tqdm
 import utils.check_folder as cf
+import matplotlib.pyplot as plt
+import math
+from sklearn.preprocessing import MultiLabelBinarizer
+import scipy.sparse as sps
+import utils.check_folder as cf
 
 def merge_consecutive_equal_actions():
     tqdm.pandas()
@@ -33,6 +38,39 @@ def _merge_consecutive_equal_actions(df):
     return df_cleaned
 
 
+attribute_dict = {}
 
-if __name__ == '__main__':
-    merge_consecutive_equal_actions()
+
+def list_item_metadata():
+    tqdm.pandas()
+    data.accomodations_df()['properties'].progress_apply(_list_item)
+    attribute_frequency_tuples = sorted(attribute_dict.items(), key=lambda x: x[1])
+
+    attribute_df = pd.DataFrame(attribute_frequency_tuples, columns=['attribute', 'frequency'])
+
+    #plot the attribute - frequency graphic
+    plot_df(attribute_df, 3, 'bar', 'attribute')
+    attribute_df.to_csv('attributes_apartament', index=False)
+
+
+def _list_item(r):
+    global attribute_dict
+    if isinstance(r, str):
+        temp = r.split('|')
+        for attribute in temp:
+            if attribute in attribute_dict.keys():
+                attribute_dict[attribute] += 1
+            else:
+                attribute_dict[attribute] = 1
+
+
+def plot_df(df, blocks_number, kind, x):
+    block_dimension = math.ceil(df.shape[0]/blocks_number)
+    for i in range(0, df.shape[0], block_dimension):
+        if i+block_dimension < df.shape[0]:
+            df[i:i+block_dimension].plot(kind=kind, x=x)
+        else:
+            df[i:df.shape[0]].plot(kind=kind, x=x)
+        plt.show()
+
+
--- preprocess.py
+++ preprocess.py
@@ -291,6 +291,44 @@ def append_missing_accomodations(mode):
     print('{} successfully updated'.format(data.ITEMS_PATH))
 
 
+def create_ICM(name='icm.npz', save_path='dataset/matrices/full/'):
+    """
+    it creates the ICM matrix taking as input the 'item_metadata.csv'
+    the matrix is saved in COO format to accomplish easy conversion to csr and csc
+    a dictionary is also saved with key = item_id and values = row of icm containing the selected item
+
+    :param name: name of the icm matrix
+    :param save_path: saving path
+    :return:
+    """
+    print("creating ICM...\n")
+    tqdm.pandas()
+    attributes_df = data.accomodations_df()
+    attributes_df['properties'] = attributes_df['properties'].progress_apply(
+        lambda x: x.split('|') if isinstance(x, str) else x)
+    attributes_df.fillna(value='', inplace=True)
+    mlb = MultiLabelBinarizer()
+    one_hot_attribute = mlb.fit_transform(attributes_df['properties'].values)
+    one_hot_dataframe = pd.DataFrame(one_hot_attribute, columns=mlb.classes_)
+
+    print("ICM created succesfully!\n")
+    print("creating dictionary...\n")
+    dict = {}
+    item_ids = attributes_df['item_id'].values
+    for i in tqdm(range(len(item_ids))):
+        dict[item_ids[i]] = i
+
+    print("saving ICM...\n")
+    cf.check_folder(save_path)
+    sps.save_npz(save_path + name, sps.coo_matrix(one_hot_dataframe.as_matrix()))
+
+    print("saving dictionary")
+    np.save(save_path + 'icm_dict.npy', dict)
+
+    print("Procedure ended succesfully!")
+
+
+
 def preprocess():
     """
     call to create the CSV files and the URM
@@ -330,6 +368,19 @@ def preprocess():
 
         append_missing_accomodations('full')
 
+    elif choice == '2':
+        pass
+    else:
+        print('Wrong choice buddy ;)')
+        exit(0)
+
+    print("Do you want to create the ICM matrix files ?")
+    print('(1) YES, sure')
+    print('(2) NO')
+    choice = input()[0]
+
+    if choice == '1':
+        create_ICM()
     elif choice == '2':
         pass
     else:
