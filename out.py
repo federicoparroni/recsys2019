3f4891e162325ad24ccfdcf15364d152d6c7d2a9
Federico Parroni <keyblade95@live.it>
2019-03-25 16:16:13 +0100

    Create sub based on mode (full or local)

--- out.py
+++ out.py
@@ -3,24 +3,30 @@ import os
 import time
 import data
 
-def create_sub(predictions, submission_name, directory='submissions'):
+def create_sub(recommendations, submission_name, mode, directory='submissions'):
     if not os.path.exists(directory):
       os.mkdir(directory)
 
-    path_time = directory + '/' + submission_name + ' ' + time.strftime('%Hh-%Mm-%Ss') + '.csv'
+    tm = time.strftime('%H-%M-%S')
+    path_time = f'{directory}/{submission_name} {tm}.csv'
+    print(f'Exporting the sub to {path_time}...')
     start = time.time()
 
-    handle_df = data.handle_df(mode='full')
-
-    # drop clickout item and impressions mantain only the 4 keys
-    handle_df.drop(handle_df.columns[4], axis=1, inplace=True)
+    handle_df = data.handle_df(mode=mode)
+    # drop impressions column from the handle to mantain only the 4 keys
+    cols_to_drop = ['impressions']
+    if mode != 'full': # if mode is local or small, drop also the reference
+        cols_to_drop.append('reference')
+    handle_df.drop(cols_to_drop, axis=1, inplace=True)
     predictions_column = []
-    for p in predictions:
-        predictions_column.append(str(p[1]).replace("[", '').replace("]","").replace(",", ""))
+    # append the recommendations joined by spaces
+    for p in recommendations:
+        predictions_column.append(' '.join(map(str,p[1])))
+    # add the new column to the dataframe
     handle_df['item_recommendations'] = predictions_column
     handle_df.to_csv(path_time, index=False)
 
     _time = time.time()-start
+    elapsed = time.strftime('%Mm %Ss', time.gmtime(_time))
     print()
-
-    print("submission created in submissions folder in {}".format(time.strftime('%Mm-%Ss', time.gmtime(_time))))
+    print(f"submission created in submissions folder in {elapsed}")
