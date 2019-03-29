2cbd33507028202ec2cb0c444fb28c3c069c517b
giovanni <giovanni.gabbolini@gmail.com>
2019-03-29 20:28:47 +0100

    fixed and commented clusterize_base + created first cluster class

--- clusterize/clusterize_base.py
+++ clusterize/clusterize_base.py
@@ -8,8 +8,14 @@ import preprocess
 
 class ClusterizeBase(ABC):
 
-    def __init__(self):
-        self.name = 'clusterbase'
+    """
+    A cluster is basically a set of session_ids to predict.
+    Extending this base class is possible to specify a set of indices (ie of sessions)
+    that a recommender should predict, other than create an ad-hoc dataset
+    """
+
+    def __init__(self, name):
+        self.name = name
 
         self.train_indices = []
         self.test_indices = []
@@ -18,10 +24,10 @@ class ClusterizeBase(ABC):
     @abstractmethod
     def _fit(self):
         """
-        save on self the following lists:
-        (:,,) -> indices of base_split/full/train to be included in cluster full training set
-        (,:,) -> indices of base_split/full/test  to be included in cluster full test set
-        (,,:) -> indices of base_split/full/test  that represents clickouts to be predicted
+        save on self the following LISTS:
+        self.train_indices -> indices of base_split/full/train to be included in cluster full training set
+        self.test_indices -> indices of base_split/full/test   to be included in cluster full test set
+        self.target_indices -> indices of base_split/full/test that represents clickouts to be predicted
         
         third argument is optional: one should specify it only in case wants to impose the clickouts
                                     to be predicted. otherwise those will be set automatically equal
@@ -36,6 +42,7 @@ class ClusterizeBase(ABC):
         folders structure inside 
         """
         self._fit()
+        print('Indices retrievied correctly ...')
 
         full_df = data.full_df()
 
@@ -53,7 +60,7 @@ class ClusterizeBase(ABC):
 
         set_train_indices = set(self.train_indices)
         set_test_indices = set(self.test_indices)
-        set_target_indices = set(self.test_indices)
+        set_target_indices = set(self.target_indices)
         
         # create FULL files:    train_df.csv  -  test_df.csv  -  target_indices.npy
         test_full_df = full_df.loc[self.test_indices].to_csv(os.path.join(full_path, 'test.csv'))
@@ -67,6 +74,7 @@ class ClusterizeBase(ABC):
         train_full_df = full_df.loc[self.train_indices]
         train_full_df.to_csv(os.path.join(full_path, 'train.csv'))
         del train_full_df
+        print('full cluster folder created...')
 
         # create LOCAL files:    train_df.csv  -  test_df.csv  -  target_indices.npy
         train_local_df = data.train_df('local')
@@ -77,7 +85,8 @@ class ClusterizeBase(ABC):
 
         test_local_df = data.test_df('local')
         valid_test_indices = set(test_local_df.index).intersection(set_test_indices)
-        test_local_df.loc[valid_test_indices].to_csv(os.path.join(local_path, 'test.csv'))
+        test_local_df = test_local_df.loc[valid_test_indices]
+        test_local_df.to_csv(os.path.join(local_path, 'test.csv'))
         if len(self.target_indices) > 0:
             np.save(os.path.join(local_path, 'target_indices'), list(valid_test_indices & set_target_indices) )
         else:
@@ -85,6 +94,7 @@ class ClusterizeBase(ABC):
             np.save(os.path.join(local_path, 'target_indices'), trgt_indices)
         del test_local_df
         del valid_test_indices
+        print('Local cluster folder created...')
 
         # create SMALL files:    train_df.csv  -  test_df.csv  -  target_indices.npy
         train_small_df = data.train_df('small')
@@ -93,11 +103,12 @@ class ClusterizeBase(ABC):
         del train_small_df
         test_small_df = data.test_df('small')
         valid_test_indices = set(test_small_df.index).intersection(set_test_indices)
-        test_small_df.loc[valid_test_indices].to_csv(os.path.join(small_path, 'test.csv'))
+        test_small_df = test_small_df.loc[valid_test_indices]
+        test_small_df.to_csv(os.path.join(small_path, 'test.csv'))
         if len(self.target_indices) > 0:
             np.save(os.path.join(small_path, 'target_indices'), list(valid_test_indices & set_target_indices) )
         else:
             trgt_indices = preprocess.get_target_indices(test_small_df)
             np.save(os.path.join(small_path, 'target_indices'), trgt_indices)
-        del test_local_df
-        
+        del test_small_df
+        print('Small cluster folder created...')
