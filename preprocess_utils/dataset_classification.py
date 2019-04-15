4fe3e3534b1393d037541c26989804013b0286f7
giovanni <giovanni.gabbolini@gmail.com>
2019-04-15 17:33:06 +0200

    cleaned up dataset creation + new features for session

--- preprocess_utils/dataset_classification.py
+++ preprocess_utils/dataset_classification.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import MultiLabelBinarizer
 import data
 import pandas as pd
 from tqdm.auto import tqdm
+import math
 tqdm.pandas()
 
 """
@@ -28,9 +29,12 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
         one_hot_features = mlb.fit_transform(accomodations_df.properties)
         one_hot_accomodations_df = pd.DataFrame(
             one_hot_features, columns=mlb.classes_)
+        one_hot_accomodations_df.columns = [
+            'accomodation feature ' + str(col) for col in one_hot_accomodations_df.columns]
         attributes_df = pd.concat([accomodations_df.drop(
             'properties', axis=1), one_hot_accomodations_df], axis=1)
         return attributes_df
+        # return accomodations_df
 
     def build_popularity(accomodations_df, df):
         popularity = {}
@@ -45,26 +49,30 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
         return popularity
 
     def func(x):
-        # TO-DO: user features! ;)
         y = x[(x['action_type'] == 'clickout item')]
         if len(y) > 0:
             clk = y.tail(1)
             head_index = x.head(1).index
+            impr = clk['impressions'].values[0].split('|')
 
             # features
             features = {'label': [], 'times_impression_appeared': [], 'time_elapsed_from_last_time_impression_appeared': [], 'impression_position': [],
-                        'steps_from_last_time_impression_appeared': [], 'kind_action_reference_appeared_last_time': [],
-                        'price': [], 'price_position': [],
-                        'item_id': [], 'popularity': [], 'other change of sort order session': 0, 'other clickout item session': 0,
-                        'other filter selection session': 0, 'other interaction item deals session': 0, 'other interaction item image session': 0,
-                        'other interaction item info session': 0, 'other interaction item rating session': 0, 'other search for destination session': 0,
-                        'other search for item session': 0, 'other search for poi session': 0, 'session length': 0,
-                        'device': '', 'filters_when_clickout': '', 'session_length': 0}
+                        'steps_from_last_time_impression_appeared': [], 'kind_action_reference_appeared_last_time': [], 'price': [], 'price_position': [],
+                        'item_id': [], 'popularity': [], 'clickout_item_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)),
+                        'interaction_item_deals_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)), 'interaction_item_image_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)),
+                        'interaction_item_info_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)), 'interaction_item_rating_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)),
+                        'search_for_item_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)), 'clickout_item_session_ref_not_in_impr': 0,
+                        'interaction_item_deals_session_ref_not_in_impr': 0, 'interaction_item_image_session_ref_not_in_impr': 0,
+                        'interaction_item_info_session_ref_not_in_impr': 0, 'interaction_item_rating_session_ref_not_in_impr': 0,
+                        'search_for_item_session_ref_not_in_impr': 0, 'session_length_in_step': 0,
+                        'device': '', 'filters_when_clickout': '', 'session_length_in_time': 0, 'sort_order_active_when_clickout': 'sorted by default'}
 
-            features['session length'] = len(x)
+            features['session_length_in_step'] = len(x)
             features['device'] = clk['device'].values[0]
-            features['filters_when_clickout'] = clk['current_filters'].values[0]
-            features['session_length'] = abs(
+            if isinstance(clk.current_filters.values[0], str):
+                features['filters_when_clickout'] = '|'.join(
+                    [x + ' filter active when clickout' for x in clk.current_filters.values[0].split('|')])
+            features['session_length_in_time'] = abs(
                 int(clk['timestamp'].values[0]) - int(x.head(1)['timestamp'].values[0]))
 
             # considering only the past!
@@ -74,6 +82,14 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
             prices = list(map(int, clk['prices'].values[0].split('|')))
             sorted_prices = prices.copy()
             sorted_prices.sort()
+
+            change_of_sort_order_actions = x[x['action_type']
+                                             == 'change of sort order']
+            if len(change_of_sort_order_actions) > 0:
+                change_of_sort_order_actions = change_of_sort_order_actions.tail(
+                    1)
+                features['sort_order_active_when_clickout'] = 'sort by ' + \
+                    change_of_sort_order_actions['reference'].values[0]
 
             references = x['reference'].values
             actions = x['action_type'].values
@@ -96,7 +112,12 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
                     features['time_elapsed_from_last_time_impression_appeared'].append(
                         int(clk['timestamp'].values[0] - row_reference['timestamp'].values[0]))
                     features['kind_action_reference_appeared_last_time'].append(
-                        'last_time_impression_appeared_as_' + row_reference['action_type'].values[0])
+                        'last_time_impression_appeared_as_' + row_reference['action_type'].values[0].replace(' ', '_'))
+                    for idx in indices:
+                        row_reference = x.head(indices[-1]+1).tail(1)
+                        features['_'.join(row_reference.action_type.values[0].split(
+                            ' ')) + '_session_ref_this_impr'][count] += 1
+
                 else:
                     features['steps_from_last_time_impression_appeared'].append(
                         0)
@@ -120,8 +141,9 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
             to_cons_indices = list(
                 set(list(range(len(actions)))) - set(not_to_cons_indices))
             for ind in to_cons_indices:
-                if ('other ' + actions[ind] + ' session') in features:
-                    features['other ' + actions[ind] + ' session'] += 1
+                if (actions[ind].replace(' ', '_') + '_session_ref_not_in_impr') in features:
+                    features[actions[ind].replace(
+                        ' ', '_') + '_session_ref_not_in_impr'] += 1
 
             return pd.DataFrame(features)
 
@@ -138,7 +160,7 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
             dataset = dataset.join(one_hot)
 
             one_hot = pd.get_dummies(
-            dataset['kind_action_reference_appeared_last_time'])
+                dataset['kind_action_reference_appeared_last_time'])
             missing = poss_actions - set(one_hot.columns)
             for e in missing:
                 one_hot[e] = 0
@@ -146,16 +168,25 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
                 ['kind_action_reference_appeared_last_time'], axis=1)
             dataset = dataset.join(one_hot)
 
-        one_hot = dataset['filters_when_clickout'].astype(
-            str).str.get_dummies()
-        missing = poss_filters - set(one_hot.columns)
-        to_drop = set(one_hot.columns) - poss_filters
-        for e in missing:
-            one_hot[e] = 0
-        for e in to_drop:
-            one_hot = one_hot.drop([e], axis=1)
-        dataset = dataset.drop(['filters_when_clickout'], axis=1)
-        dataset = dataset.join(one_hot)
+            one_hot = pd.get_dummies(
+                dataset['sort_order_active_when_clickout'])
+            missing = poss_sort_orders - set(one_hot.columns)
+            for e in missing:
+                one_hot[e] = 0
+            dataset = dataset.drop(
+                ['sort_order_active_when_clickout'], axis=1)
+            dataset = dataset.join(one_hot)
+
+            one_hot = dataset['filters_when_clickout'].astype(
+                str).str.get_dummies()
+            missing = poss_filters - set(one_hot.columns)
+            to_drop = set(one_hot.columns) - poss_filters
+            for e in missing:
+                one_hot[e] = 0
+            for e in to_drop:
+                one_hot = one_hot.drop([e], axis=1)
+            dataset = dataset.drop(['filters_when_clickout'], axis=1)
+            dataset = dataset.join(one_hot)
 
         dataset = dataset.reset_index().drop(['level_2'], axis=1)
         dataset = pd.merge(dataset, one_hot_accomodation, on=['item_id'])
@@ -179,11 +210,16 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
     popularity_df = build_popularity(accomodations_df, full)
     poss_filters = []
     for f in full[~full['current_filters'].isnull()]['current_filters'].values:
-        poss_filters += f.split('|')
+        poss_filters += [x +
+                         ' filter active when clickout' for x in f.split('|')]
     poss_filters = set(poss_filters)
     poss_devices = set(list(full['device'].values))
-    poss_actions = set(['last_time_impression_appeared_as_' +
-                        x for x in full['action_type'].values])
+    poss_actions = set(['last_time_reference_did_not_appeared', 'last_time_impression_appeared_as_clickout_item',
+                        'last_time_impression_appeared_as_interaction_item_deals', 'last_time_impression_appeared_as_interaction_item_image',
+                        'last_time_impression_appeared_as_interaction_item_info', 'last_time_impression_appeared_as_interaction_item_rating',
+                        'last_time_impression_appeared_as_search_for_item'])
+    poss_sort_orders = set(['sort by ' + x for x in full[full['action_type']
+                                                         == 'change of sort order']['reference'].values])
 
     # build in chunk
     count_chunk = 0
@@ -212,4 +248,4 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
 
 
 if __name__ == "__main__":
-    build_dataset(mode='small', algo='catboost')
+    build_dataset(mode='small', algo='xgboost')
