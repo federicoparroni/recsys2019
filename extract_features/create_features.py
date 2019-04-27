fee9ed65222b016ba3470186667bca71e393b63a
Federico Parroni <keyblade95@live.it>
2019-04-27 09:59:47 +0200

    Added menu to choose mode and cluster in create features

--- extract_features/create_features.py
+++ extract_features/create_features.py
@@ -1,3 +1,4 @@
+import utils.menu as menu
 from extract_features.actions_involving_impression_session import ActionsInvolvingImpressionSession
 from extract_features.mean_price_clickout import MeanPriceClickout
 from extract_features.label import ImpressionLabel
@@ -12,13 +13,17 @@ from extract_features.timing_from_last_interaction_impression import TimingFromL
 from extract_features.last_action_involving_impression import LastInteractionInvolvingImpression
 from extract_features.session_actions_num_ref_diff_from_impressions import SessionActionNumRefDiffFromImpressions
 
-# define all the features to be made
-features = [ActionsInvolvingImpressionSession, MeanPriceClickout, ImpressionLabel, ImpressionPositionSession,
-            SessionLength, SessionDevice, SessionFilterActiveWhenClickout, SessionSortOrderWhenClickout,
-            ImpressionPriceInfoSession, TimesUserInteractedWithImpression, TimingFromLastInteractionImpression,
-            LastInteractionInvolvingImpression, SessionActionNumRefDiffFromImpressions]
+if __name__ == "__main__":    
+    mode = menu.mode_selection()
+    cluster = menu.cluster_selection()
 
-# create all the features defined in the array 'features'
-for feature_name in features:
-    feature = feature_name(mode='full', cluster='no_cluster')
-    feature.save_feature()
+    # define all the features to be made
+    features = [ActionsInvolvingImpressionSession, MeanPriceClickout, ImpressionLabel, ImpressionPositionSession,
+                SessionLength, SessionDevice, SessionFilterActiveWhenClickout, SessionSortOrderWhenClickout,
+                ImpressionPriceInfoSession, TimesUserInteractedWithImpression, TimingFromLastInteractionImpression,
+                LastInteractionInvolvingImpression, SessionActionNumRefDiffFromImpressions]
+
+    # create all the features defined in the array 'features'
+    for feature_name in features:
+        feature = feature_name(mode=mode, cluster=cluster)
+        feature.save_feature()
