2d2cddab695767dd755bbad2075c99c1e4cde9ae
Federico Parroni <keyblade95@live.it>
2019-04-28 14:27:52 +0200

    File to create the dataset for RNN

--- extract_features/feature_base.py
+++ extract_features/feature_base.py
@@ -73,7 +73,6 @@ class FeatureBase(ABC):
         """ Join this feature to the specified dataframe. The default implementation will join based on the
         common column between the 2 dataframes. Override to provide a custom join logic. """
         feature_df = self.read_feature(one_hot=one_hot)
-        #common_cols = list(set(df.columns) & set(feature_df.columns))
         return df.merge(feature_df, how='left')
 
     def read_feature(self, one_hot=False):
