Added batch normalization to RNN, bug fixed in activation type for only 1 dense layers

recommenders/RNN.py
@@ -6,7 +6,7 @@ import time
 from abc import abstractmethod
 
 from keras.models import Sequential, load_model
-from keras.layers import Dense, LSTM, GRU, Embedding
+from keras.layers import Dense, LSTM, GRU, Embedding, BatchNormalization, Activation, Dropout
 from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
 from keras import metrics
 
@@ -24,7 +24,7 @@ from utils.telegram_bot import TelegramBotKerasCallback
 class RecurrentRecommender(RecommenderBase):
     
     def __init__(self, dataset, cell_type, num_recurrent_layers, num_recurrent_units, num_dense_layers,
-                use_generator=True, validation_split=0.15,
+                use_generator=True, validation_split=0.15, use_batch_normalization=True,
                 loss='mean_squared_error', optimizer='rmsprop', class_weights=[],
                 checkpoints_path=None, tensorboard_path=None):
         """ Create the recurrent model
@@ -71,13 +71,19 @@ class RecurrentRecommender(RecommenderBase):
         for i in range(num_recurrent_layers-1):
             self.model.add( CELL(num_recurrent_units, dropout=0.2,recurrent_dropout=0.2, return_sequences=(i < num_recurrent_layers-1) ))
 
-        if num_dense_layers == 1:
-            self.model.add( Dense(output_size, activation='softmax') )
-        else:
+        if num_dense_layers > 1:
             dense_neurons = np.linspace(num_recurrent_units, output_size, num_dense_layers)
-            for i,n in enumerate(dense_neurons):
-                act = 'softmax' if i == len(dense_neurons)-1 else 'sigmoid'
-                self.model.add( Dense(int(n), activation=act) )
+            for n in dense_neurons[:-1]:
+                self.model.add( Dense(int(n), activation=None) )
+                if use_batch_normalization:
+                    self.model.add( BatchNormalization() )
+                self.model.add( Activation('relu') )
+        
+        self.model.add( Dense(output_size, activation=None) )
+        if use_batch_normalization:
+            self.model.add( BatchNormalization() )
+        self.model.add( Activation('softmax') )
+        self.model.add( Dropout(rate=0.1) )
         
         self.model.compile(sample_weight_mode='temporal', loss=loss, optimizer=optimizer, metrics=['accuracy', self.mrr])
 