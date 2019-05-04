c138942698a35d0e00036f56cfc2e2f2f664945b
Federico Parroni <keyblade95@live.it>
2019-05-04 17:13:01 +0200

    Ask to overwrite

--- preprocess_utils/create_dataset_for_classification_rnn.py
+++ preprocess_utils/create_dataset_for_classification_rnn.py
@@ -16,20 +16,26 @@ if __name__ == "__main__":
     cluster_name = 'cluster_recurrent'
 
     # create the cluster
-    print('Creating the cluster...')
-    cluster = ClusterRecurrent()
-    cluster.save(mode)
-    print()
+    cluster_choice = menu.yesno_choice('Do you want to create the cluster?', lambda: True, lambda: False)
+    if cluster_choice:
+        print('Creating the cluster...')
+        cluster = ClusterRecurrent()
+        cluster.save(mode)
+        print()
+
+    only_test = False
+    if mode == 'full':
+        only_test = menu.yesno_choice('Do you want to create only the test dataset?', lambda: True, lambda: False)
 
     # create the features to join
     ref_pos_next_clk_feat = ReferencePositionInNextClickoutImpressions(mode=mode, cluster=cluster_name)
-    ref_pos_next_clk_feat.save_feature(overwrite_if_exists=True)
+    ref_pos_next_clk_feat.save_feature()
     print()
 
     features = [ref_pos_next_clk_feat]
     # create the tensors dataset
-    print('Creating the dataset...')
+    print('Creating the dataset ({})...'.format(mode))
     sess2vec.create_dataset_for_classification(mode, cluster_name, pad_sessions_length=sess_length,
-                                                add_item_features=False, features=features)
+                                                add_item_features=False, features=features, only_test=only_test)
 
 
