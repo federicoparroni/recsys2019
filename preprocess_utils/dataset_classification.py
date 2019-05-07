038b0bf6a8bd765d19363eec4891191d9d7ca736
Daniele <danmontesi@gmail.com>
2019-05-07 10:41:10 +0200

    Fix dataset for xgboost/catboost

--- preprocess_utils/dataset_classification.py
+++ preprocess_utils/dataset_classification.py
@@ -20,12 +20,6 @@ the accomodation_ids are the ones showing up in the impressions
 label is 1 in case the accomodation is the one clicked in the clickout
 """
 
-# import os
-
-# os.chdir("../")
-# print(os.getcwd())
-
-
 def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
     # build the onehot of accomodations attributes
     def one_hot_of_accomodation(accomodations_df):
@@ -74,9 +68,10 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
 
         return popularity
 
+
     def get_frenzy_and_avg_time_per_step(x, clk):
-        if len(x) > 0:
-            session_actions_num = int(clk.step.values[0])
+        session_actions_num = int(clk.step.values[0])
+        if session_actions_num > 1:
 
             clickout_tm = int(clk.timestamp.values[0])
             time_length = clickout_tm - int(x.head(1).timestamp.values[0])
@@ -113,15 +108,12 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
             head_index = x.head(1).index
             impr = clk['impressions'].values[0].split('|')
 
-            # !! considering only the past for all non-test sessions! !!
-            x = x.loc[head_index.values[0]:clk.index.values[0] - 1]
-
             # features
             features = {'label': [], 'times_impression_appeared': [],
                         'time_elapsed_from_last_time_impression_appeared': [], 'impression_position': [],
                         'steps_from_last_time_impression_appeared': [], 'kind_action_reference_appeared_last_time': [],
-                        'price': [], 'price_position': [],
-                        'item_id': [], 'popularity': [], 'impression_position_wrt_last_interaction': [],
+                        'price': [], 'price_position': [], 'item_id': [], 'popularity': [],
+                        'impression_position_wrt_last_interaction': [],
                         'impression_position_wrt_second_last_interaction': [],
                         'clickout_item_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)),
                         'interaction_item_deals_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)),
@@ -129,36 +121,32 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
                         'interaction_item_info_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)),
                         'interaction_item_rating_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)),
                         'search_for_item_session_ref_this_impr': list(np.zeros(len(impr), dtype=np.int)),
-                        'clickout_item_session_ref_not_in_impr': 0,
-                        'interaction_item_deals_session_ref_not_in_impr': 0,
+                        'clickout_item_session_ref_not_in_impr': 0, 'interaction_item_deals_session_ref_not_in_impr': 0,
                         'interaction_item_image_session_ref_not_in_impr': 0,
                         'interaction_item_info_session_ref_not_in_impr': 0,
                         'interaction_item_rating_session_ref_not_in_impr': 0,
-                        'search_for_item_session_ref_not_in_impr': 0, 'session_length_in_step': 0,
-                        'device': '', 'filters_when_clickout': '', 'session_length_in_time': 0,
+                        'search_for_item_session_ref_not_in_impr': 0, 'session_length_in_step': int(clk.step.values[0]),
+                        'device': clk['device'].values[0], 'filters_when_clickout': '', 'session_length_in_time': 0,
                         'sort_order_active_when_clickout': 'sorted by default'}
-
-            features['session_length_in_step'] = int(clk.step.values[0])
-            features['device'] = clk['device'].values[0]
 
             if isinstance(clk.current_filters.values[0], str):
                 features['filters_when_clickout'] = '|'.join(
                     [x + ' filter active when clickout' for x in clk.current_filters.values[0].split('|')])
-            else:
-                features['filters_when_clickout'] = 'no filter when clickout'
+            features['session_length_in_time'] = abs(
+                int(clk['timestamp'].values[0]) - int(x.head(1)['timestamp'].values[0]))
 
-            if len(x) > 0:
-                features['session_length_in_time'] = int(clk['timestamp'].values[0]) - int(
-                    x.head(1)['timestamp'].values[0])
-
-                features['timing_last_action_before_clk'] = int(clk.timestamp.values[0]) - int(
-                    x.tail(1).timestamp.values[0])
+            if len(x) > 1:
+                features['timing_last_action_before_clk'] = int(x.tail(2).timestamp.values[1]) - int(x.tail(2).timestamp.values[0])
             else:
-                features['session_length_in_time'] = -1
                 features['timing_last_action_before_clk'] = -1
 
-            features['time_per_step'], features['frenzy_factor'] = get_frenzy_and_avg_time_per_step(
-                x, clk)
+            features['time_per_step'], features['frenzy_factor'] = get_frenzy_and_avg_time_per_step(x, clk)
+
+            # considering only the past!
+            x = x.loc[head_index.values[0]:clk.index.values[0]-1]
+
+            x_only_clickouts = x[(x.action_type == 'clickout item')]
+            features['times_doubleclickout_on_item'] = len(x_only_clickouts) - len(set(x_only_clickouts.reference.values))
 
             poi_search_df = x[x.action_type == 'search for poi']
             if poi_search_df.shape[0] > 0:
@@ -195,7 +183,6 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
                     1)
                 features['sort_order_active_when_clickout'] = 'sort by ' + \
                                                               str(change_of_sort_order_actions['reference'].values[0])
-
             references = x['reference'].values
             actions = x['action_type'].values
 
@@ -227,7 +214,6 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
             # Start features impressions
             for i in impr:
                 indices = np.where(references == str(i))[0]
-
                 not_to_cons_indices += list(indices)
                 features['impression_position'].append(count + 1)
 
@@ -422,7 +408,7 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
     # build in chunk
     # avoid session truncation, explicitly specify how many session you want in a chunk
     count_chunk = 0
-    session_to_consider_in_chunk = 200
+    session_to_consider_in_chunk = 2000
     full = full.reset_index(drop=True)
     session_indices = list(
         full[['user_id']].drop_duplicates(keep='last').index.values)
@@ -450,7 +436,6 @@ def build_dataset(mode, cluster='no_cluster', algo='xgboost'):
             p2 = Process(target=save_features, args=(
                 features, count_chunk, target_session_id, target_user_id,))
             p2.start()
-
 
 if __name__ == "__main__":
     build_dataset(mode='full', cluster='no_cluster', algo='xgboost')
