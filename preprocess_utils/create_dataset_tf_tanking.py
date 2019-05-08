a2cb1cdb33edded18a99fbb89724e1ddde558394
Unknown <damicoedoardo@virgilio.it>
2019-05-08 11:02:21 +0200

    changed run on mode local

--- preprocess_utils/create_dataset_tf_tanking.py
+++ preprocess_utils/create_dataset_tf_tanking.py
@@ -220,7 +220,7 @@ if __name__ == '__main__':
                       TimingFromLastInteractionImpression, TimesUserInteractedWithImpression,
                       ImpressionPositionSession, LastInteractionInvolvingImpression,
                       TimesImpressionAppearedInClickoutsSession, MeanPriceClickout, SessionLength,
-                      TimeFromLastActionBeforeClk, FrenzyFactorSession, PricePositionInfoInteractedReferences,
+                      TimeFromLastActionBeforeClk, PricePositionInfoInteractedReferences,
                       SessionDevice, SessionFilterActiveWhenClickout, SessionSortOrderWhenClickout,
                       ImpressionFeature]
 
