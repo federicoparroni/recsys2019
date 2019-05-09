909cc0c34b74834a4796fdb0c66d2937bc150494
gabbo <giovanni.gabbolini@gmail.com>
2019-05-09 12:48:46 +0200

    optimized and fixed features

--- extract_features/session_actions_num_ref_diff_from_impressions.py
+++ extract_features/session_actions_num_ref_diff_from_impressions.py
@@ -8,10 +8,10 @@ tqdm.pandas()
 class SessionActionNumRefDiffFromImpressions(FeatureBase):
 
     """
-    actions that happened:
-    | user_id | session_id | session_action_num_ref_diff_from_impressions
-    session_action_num_ref_diff_from_impressions is string, in the format: action1 | action2 | action3 ...
-    it is possible to have more times a action as replicated
+    | user_id | session_id | actions_num_ref_diff_from_impressions_clickout_item | actions_num_ref_diff_from_impressions_interaction_item_deals | ..
+
+    tells how many time the user clicked during a session in an action with numeric ref diff from all the other
+    impressions
     """
 
     def __init__(self, mode, cluster='no_cluster'):
@@ -25,37 +25,56 @@ class SessionActionNumRefDiffFromImpressions(FeatureBase):
             c = 0
             y = x[x['action_type'] == 'clickout item']
             if len(y) > 0:
-                r = pd.DataFrame({
-                    'clickout item': [0],
-                    'interaction item deals': [0],
-                    'interaction item image': [0],
-                    'interaction item info': [0],
-                    'interaction item rating': [0],
-                    'search for item': [0],
-                    'no action': [0]})
+                clickout_item = 0
+                interaction_item_deals = 0
+                interaction_item_image = 0
+                interaction_item_info = 0
+                interaction_item_rating = 0
+                search_for_item = 0
+                no_action = 0
                 clk = y.tail(1)
-                head_index = x.head(1).index
                 impr = clk.impressions.values[0]
-                x = x.loc[head_index.values[0]:clk.index.values[0]-1]
-                df_only_numeric = x[pd.to_numeric(x['reference'], errors='coerce').notnull()][[
-                    "reference", "action_type", "frequence"]]
-                df_only_numeric = df_only_numeric.reset_index(drop=True)
-                refs = df_only_numeric.reference.values
+                x = x[x['step'] < int(clk['step'])]
+                df_only_numeric = x[pd.to_numeric(x['reference'], errors='coerce').notnull()]
+                refs = list(df_only_numeric.reference.values)
+                freqs = list(df_only_numeric.frequence.values)
+                actions = list(df_only_numeric.action_type.values)
                 for i in range(len(refs)):
                     if refs[i] not in impr:
+                        action_i = actions[i]
+                        if action_i == 'clickout item':
+                            clickout_item += int(freqs[i])
+                        elif action_i == 'interaction item deals':
+                            interaction_item_deals += int(freqs[i])
+                        elif action_i == 'interaction item info':
+                            interaction_item_info += int(freqs[i])
+                        elif action_i == 'interaction item image':
+                            interaction_item_image += int(freqs[i])
+                        elif action_i == 'interaction item rating':
+                            interaction_item_rating += int(freqs[i])
+                        elif action_i == 'search for item':
+                            search_for_item += int(freqs[i])
                         c += 1
-                        r[df_only_numeric.loc[i].action_type] += int(df_only_numeric.loc[i].frequence)
                 if c == 0:
-                    r['no action'] += 1
-                return r
+                    no_action += 1
+                return [(clickout_item, interaction_item_deals, interaction_item_image,
+                        interaction_item_info, interaction_item_rating, search_for_item, no_action)]
 
         train = data.train_df(mode=self.mode, cluster=self.cluster)
         test = data.test_df(mode=self.mode, cluster=self.cluster)
         df = pd.concat([train, test])
-        s = df.groupby(
-            ['user_id', 'session_id']).progress_apply(func)
-        s = s.reset_index()
-        s = s.drop(['level_2'], axis=1)
+        df = df[["session_id", "user_id", "reference", "step", "action_type", "frequence", "impressions"]]
+        s = df.groupby(['user_id', 'session_id']).progress_apply(func)
+        s = s.apply(pd.Series).reset_index().melt(id_vars=['user_id', 'session_id'], value_name='tuple').sort_values(
+            by=['user_id', 'session_id']).dropna()
+        s[['actions_num_ref_diff_from_impressions_clickout_item',
+           'actions_num_ref_diff_from_impressions_interaction_item_deals',
+           'actions_num_ref_diff_from_impressions_interaction_item_image',
+           'actions_num_ref_diff_from_impressions_interaction_item_info',
+           'actions_num_ref_diff_from_impressions_interaction_item_rating',
+           'actions_num_ref_diff_from_impressions_search_for_item',
+           'actions_num_ref_diff_from_impressions_no_action']] = pd.DataFrame(s['tuple'].tolist(), index=s.index)
+        s = s.drop(['variable', 'tuple'], axis=1)
         return s
 
 
