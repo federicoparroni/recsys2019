6d50448f72632fc45baf661ea0f2559be6080906
Unknown <damicoedoardo@virgilio.it>
2019-05-09 16:15:49 +0200

    hera no patience tfranking

--- utils/best_checkpoint_copier.py
+++ utils/best_checkpoint_copier.py
@@ -95,14 +95,12 @@ class BestCheckpointCopier(tf.estimator.Exporter):
       dataset = tf.data.Dataset.from_tensor_slices((features, labels))
       return dataset.batch(batch_size)
 
-    def create_sub(estimator, checkpoint_path, eval_result, batch_size=64, patience=0.005):
+    def create_sub(estimator, checkpoint_path, eval_result, batch_size=256, patience=0.003):
       # now works also for local and small it will create a sub
       # create a sub only if the MMR is > 0.65
       if (self.mode == 'full') or (self.mode == 'local'):
         eval_result_f = eval_result['metric/mrr']
-        print(self.min_mrr)
-        print(eval_result_f)
-        if eval_result_f>self.min_mrr+patience:
+        if True:#eval_result_f>self.min_mrr+patience:
           # set as new threshold the new mrr
           self.min_mrr = eval_result_f
 
