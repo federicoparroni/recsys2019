71efb573231f39c2362247c93f97d2f5c29f1801
russointroitoa <russointroitoa@gmail.com>
2019-05-12 19:00:39 +0200

    Adding no reference session cluster

--- evaluate/SubEvaluator.py
+++ evaluate/SubEvaluator.py
@@ -215,6 +215,19 @@ class SubEvaluator():
             new_gt.to_csv(self.data_directory.joinpath('gt_clusters/ground_truth_small.csv'), encoding='utf-8', index=False)
             print('Done.')
 
+        if not os.path.isfile(self.data_directory.joinpath('gt_clusters/gt_no_num_reference_sessions.csv')):
+            print('Creating cluster of sessions with no numeric reference...')
+            group = df_test.groupby('session_id')['reference'].progress_apply(
+                lambda x: False if x.str.isnumeric().any() else True)
+            sess_list = list(group.index)
+            index_list = []
+            for i in range(len(group)):
+                if group.iloc[i] == True:
+                    index_list.append(i)
+            no_ref_sessions = [sess_list[i] for i in index_list]
+            new_gt = df_gt[df_gt['session_id'].isin(no_ref_sessions)]
+            new_gt.to_csv(self.data_directory.joinpath('gt_clusters/gt_no_num_reference_sessions.csv'), encoding='utf-8', index=False)
+
 if __name__=='__main__':
     sub_evaluator = SubEvaluator('xgboostlocal.csv')
     sub_evaluator.run()
