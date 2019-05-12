79dad51d2d4582268a34b16c86ad0427b4f33f3e
Daniele <danmontesi@gmail.com>
2019-05-12 12:39:18 +0200

    Prepare catboost for valid

--- recommenders/catboost_rank.py
+++ recommenders/catboost_rank.py
@@ -11,8 +11,8 @@ from catboost import CatBoost, Pool
 from copy import deepcopy
 import pickle
 import pandas as pd
-tqdm.pandas()
 
+tqdm.pandas()
 
 
 class CatboostRanker(RecommenderBase):
@@ -25,8 +25,9 @@ class CatboostRanker(RecommenderBase):
 
     def __init__(self, mode, cluster='no_cluster', learning_rate=0.15, iterations=200, max_depth=10, reg_lambda=2,
                  colsample_bylevel=1,
-                 custom_metric='AverageGain:top=1', algo='xgboost', verbose=False, include_test=False, file_to_load=None,
-                 file_to_store=None, limit_trees=False, features_to_one_hot = None):
+                 custom_metric='AverageGain:top=1', algo='xgboost', verbose=False, include_test=False,
+                 file_to_load=None,
+                 file_to_store=None, limit_trees=False, features_to_one_hot=None):
         """
         :param mode:
         :param cluster:
@@ -61,12 +62,13 @@ class CatboostRanker(RecommenderBase):
             'boosting_type': 'Plain',
             'loss_function': 'QuerySoftMax',
             'train_dir': 'QuerySoftMax',
+            'logging_level': 'Verbose'
         }
 
         # create hyperparameters dictionary
-        self.hyperparameters_dict = {'iterations': (150, 150),
-                                     'max_depth': (9.5, 11.3),
-                                     'learning_rate': (0.1, 0.1),
+        self.hyperparameters_dict = {'iterations': (50, 50),
+                                     'max_depth': (10.5, 11),
+                                     'learning_rate': (0.15, 0.15),
                                      'reg_lambda': (6.5, 6.75),
                                      }
 
@@ -94,13 +96,13 @@ class CatboostRanker(RecommenderBase):
 
         model = CatBoost(parameters)
         start = time.time()
+
         model.fit(train_pool, eval_set=test_pool, plot=False)
 
         end = time.time()
         print('With {} iteration, training took {} sec'.format(parameters['iterations'], end - start))
 
         return model
-
 
     def get_feature_importance(self, additional_params=None, train_pool=None):
         parameters = deepcopy(self.default_parameters)
@@ -113,7 +115,7 @@ class CatboostRanker(RecommenderBase):
 
         return features_imp
 
-    def fit(self, is_train = True):
+    def fit(self, is_train=True):
 
         if self.file_to_load is not None:
             # --- To load model ---
@@ -156,7 +158,6 @@ class CatboostRanker(RecommenderBase):
             test_df = data.classification_test_df(
                 mode=self.mode, sparse=False, cluster=self.cluster)
 
-
             test_df = test_df.sort_values(by=['user_id', 'session_id'])
 
             test_df['id'] = test_df.groupby(['user_id', 'session_id']).ngroup()
@@ -177,7 +178,7 @@ class CatboostRanker(RecommenderBase):
 
         if is_train:
             self.ctb = self.fit_model(train_pool=train_with_weights,
-                                  test_pool=test_with_weights)
+                                      test_pool=test_with_weights)
         else:
             self.ctb = self.get_feature_importance(train_pool=train_with_weights)
         print('fit done')
@@ -213,7 +214,7 @@ class CatboostRanker(RecommenderBase):
             cat_features=self.categorical_features
         )
 
-        if self.limit_trees and self.limit_trees>0:
+        if self.limit_trees and self.limit_trees > 0:
             scores = self.ctb.predict(test_with_weights, ntree_end=self.limit_trees)
         else:
             scores = self.ctb.predict(test_with_weights)
@@ -266,11 +267,10 @@ class CatboostRanker(RecommenderBase):
 
         feat_imp = list(self.ctb)
         tuples_features = list(zip(self.train_features, feat_imp))
-        sorted(tuples_features, key = lambda x: x[1])
+        sorted(tuples_features, key=lambda x: x[1])
 
         features = [el[0] for el in list(zip(*tuples_features))]
         score = [el[1] for el in list(zip(*tuples_features))]
-
 
         x_pos = np.arange(len(features))
 
@@ -279,7 +279,7 @@ class CatboostRanker(RecommenderBase):
         plt.ylabel('Popularity Score')
         plt.show()
 
-    def iterations_validation(self, max_trees, range_step = 25, mode = 'auto'):
+    def iterations_validation(self, max_trees, range_step=25, mode='auto'):
         if self.ctb is None:
             self.fit()
 
@@ -287,13 +287,8 @@ class CatboostRanker(RecommenderBase):
 
         test_df.drop(['user_id', 'session_id'], inplace=True, axis=1)
 
-        self.predictions = []
-        self.scores_batch = []
-
-        test_df.groupby('trg_idx', as_index=False).progress_apply(self.func)
-
         if mode == 'auto':
-            list_num_trees = [max_trees-i*25 for i in range(max_trees)]
+            list_num_trees = [max_trees - i * range_step for i in range(max_trees)]
 
             for trees in list_num_trees:
                 self.set_limit_trees(trees)
@@ -303,10 +298,13 @@ class CatboostRanker(RecommenderBase):
                 test_df.groupby('trg_idx', as_index=False).progress_apply(self.func)
 
                 MRR = self.compute_MRR(self.predictions)
-                HERA.send_message('evaluating recommender {} on {}. Iterations used {}\n MRR is: {}\n\n'.format(self.name, self.cluster, trees, MRR))
-
+                HERA.send_message(
+                    'evaluating recommender {} on {}. Iterations used {}\n MRR is: {}\n\n'.format(self.name,
+                                                                                                  self.cluster, trees,
+                                                                                                  MRR))
         while True:
-            #Getting user input
+
+            # Getting user input
             while True:
                 trees = input("How many iterations?")
                 try:
@@ -323,7 +321,6 @@ class CatboostRanker(RecommenderBase):
             HERA.send_message(
                 'evaluating recommender {} on {}. Iterations used {}\n MRR is: {}\n\n'.format(self.name, self.cluster,
                                                                                               trees, MRR))
-
 
     def get_preprocessed_dataset(self, mode):
         """
@@ -343,8 +340,7 @@ class CatboostRanker(RecommenderBase):
                 mode=self.mode, sparse=False, cluster=self.cluster, algo=self.algo).copy()
 
         classification_df = classification_df.reindex(
-            'user_id,session_id,label,times_impression_appeared,time_elapsed_from_last_time_impression_appeared,impression_position,steps_from_last_time_impression_appeared,price,price_position,impression_position_wrt_last_interaction,impression_position_wrt_second_last_interaction,clickout_item_session_ref_this_impr,interaction_item_deals_session_ref_this_impr,interaction_item_image_session_ref_this_impr,interaction_item_info_session_ref_this_impr,interaction_item_rating_session_ref_this_impr,search_for_item_session_ref_this_impr,session_length_in_step,session_length_in_time,time_per_step,frenzy_factor,average_price_position,avg_price_interacted_item,avg_pos_interacted_items_in_impressions,pos_last_interaction_in_impressions,search_for_poi_distance_from_last_clickout,search_for_poi_distance_from_first_action,change_sort_order_distance_from_last_clickout,change_sort_order_distance_from_first_action,time_passed_before_clk'.split(
-                ','), axis=1)
+            'user_id,session_id,label,times_impression_appeared,time_elapsed_from_last_time_impression_appeared,impression_position,steps_from_last_time_impression_appeared,price,price_position,popularity,impression_position_wrt_last_interaction,impression_position_wrt_second_last_interaction,clickout_item_session_ref_this_impr,interaction_item_deals_session_ref_this_impr,interaction_item_image_session_ref_this_impr,interaction_item_info_session_ref_this_impr,interaction_item_rating_session_ref_this_impr,search_for_item_session_ref_this_impr,clickout_item_session_ref_not_in_impr,interaction_item_deals_session_ref_not_in_impr,interaction_item_image_session_ref_not_in_impr,interaction_item_info_session_ref_not_in_impr,interaction_item_rating_session_ref_not_in_impr,search_for_item_session_ref_not_in_impr,session_length_in_step,session_length_in_time,time_per_step,frenzy_factor,average_price_position,avg_price_interacted_item,avg_pos_interacted_items_in_impressions,pos_last_interaction_in_impressions,search_for_poi_distance_from_last_clickout,search_for_poi_distance_from_first_action,change_sort_order_distance_from_last_clickout,change_sort_order_distance_from_first_action,time_passed_before_clk'.split(','), axis=1)
 
         if len(self.features_to_drop) > 0:
             classification_df.drop(self.features_to_drop, axis=1, inplace=True)
@@ -377,7 +373,7 @@ class CatboostRanker(RecommenderBase):
         return classification_df
 
 
-
 if __name__ == '__main__':
-    model = CatboostRanker(mode='small', cluster='no_cluster', iterations=100, include_test=False, file_to_load='cat_600.sav', algo='xgboost')
-    model.iterations_validation(10, range_step=2)
+    model = CatboostRanker(mode='full', cluster='no_cluster', iterations=100, include_test=False,
+                        algo='xgboost')
+    model.run()
