0f2c39d523dea03c037398513922fcd9d1e0b676
Daniele <danmontesi@gmail.com>
2019-05-13 18:27:41 +0200

    add main

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from recommenders.hybrid.borda_hybrid import Borda_Hybrid
-from recommenders.hybrid.Probabilistic_Hybrid import Probabilistic_Hybrid
-from evaluate.SubEvaluator import SubEvaluator
-#model = Probabilistic_Hybrid(mode='local')
-#model.run()
+from recommenders.catboost_rank import CatboostRanker
 
-evaluator = SubEvaluator('xgboostlocal.csv')
-evaluator.run()
+from recommenders.hybrid.hybrid_impression_scores import HybridImpressionScores
+from recommenders.only_test_based.lazy_user_recommender import LazyUserRecommender
+from recommenders.only_test_based.ordered_impression_based import OrderedImpressionRecommender
+
+m1 = LazyUserRecommender(mode='small', cluster='no_cluster')
+scores_1 = m1.get_scores_batch()
+
+m2 = OrderedImpressionRecommender(mode='small', cluster='no_cluster')
+scores_2 = m2.get_scores_batch()
+
+
+evaluator = HybridImpressionScores('small', 'no_cluster', [scores_1, scores_2], [0.5, 0.5], normalization_mode='MAX_ROW')
+evaluator.evaluate()
