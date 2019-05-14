dc874e5a772508c183bf344f9b7547ee3c59cdf4
Federico Parroni <keyblade95@live.it>
2019-05-14 10:01:12 +0200

    Removed user_id and session_id from feature reference_position_in_last_clickout impressions

--- extract_features/reference_position_in_last_clickout_impressions.py
+++ extract_features/reference_position_in_last_clickout_impressions.py
@@ -14,7 +14,7 @@ class ReferencePositionInLastClickoutImpressions(FeatureBase):
     """
     Extracts the position of the row reference inside the last clickout impressions.
     If the reference is not present in the last clickout impressions, the position will be -1
-    | index | user_id | session_id | ref_pos
+    | index | ref_pos
     ref_pos is a number between 0-24 or -1
     """
 
@@ -32,13 +32,13 @@ class ReferencePositionInLastClickoutImpressions(FeatureBase):
         df = data.full_df()
 
         df = df.sort_index()
-        # find the clickout rows
+        # find the last clickout rows
         last_clickout_idxs = find_last_clickout_indices(df)
         clickout_rows = df.loc[last_clickout_idxs, ['user_id','session_id','action_type','impressions']]
         clickout_rows['impression_list'] = clickout_rows.impressions.str.split('|')
         clickout_rows = clickout_rows.drop('impressions', axis=1)
 
-        # find the interaction with numeric reference
+        # find the interactions with numeric reference
         reference_rows = df[['user_id','session_id','reference','action_type']]
         reference_rows = reference_rows[df.reference.str.isnumeric() & (df.action_type != 'clickout item')]
         reference_rows = reference_rows.drop('action_type', axis=1)
@@ -63,7 +63,7 @@ class ReferencePositionInLastClickoutImpressions(FeatureBase):
                 except:
                     pass
         
-        return reference_rows.drop('reference', axis=1)
+        return reference_rows.drop(['user_id','session_id','reference'], axis=1)
 
     def post_loading(self, df):
         # drop the one-hot column -1, representing a non-numeric reference or a reference not present
@@ -74,7 +74,7 @@ class ReferencePositionInLastClickoutImpressions(FeatureBase):
 
     def join_to(self, df, one_hot=True):
         """ Join this feature to the specified dataframe """
-        feature_df = self.read_feature(one_hot=one_hot).drop(['user_id','session_id'],axis=1)
+        feature_df = self.read_feature(one_hot=one_hot)
         feature_cols = feature_df.columns
         res_df = df.merge(feature_df, how='left', left_index=True, right_index=True)
         if one_hot:
