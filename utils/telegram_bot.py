03a72f9c6d8f5cea4a5428cbec4cb0178cb59573
Federico Parroni <keyblade95@live.it>
2019-05-23 23:25:16 +0200

    Added support for multiple telegram accounts

--- utils/telegram_bot.py
+++ utils/telegram_bot.py
@@ -1,12 +1,45 @@
 import telepot
 import keras
 
-chat_id = -1001481984580
-token = '819065046:AAFee77GqSpq8XBzmEnAMybLqOHuy6PJ_bg'
-HERA_BOT = telepot.Bot(token=token)
+"""
+To create a new private channel, you will need 2 things:
+1) A token bot: you can create by BotFather inside telegram. From the bot, you have to get the 'token'
+    following the on-screen commands
+2) A chat id: start the bot on telegram, send any message to the bot (important!) and then go to this address:
+    https://api.telegram.org/bot<yourtoken>/getUpdates
+    (replace <yourtoken> with the one got at the previous step)
+    Copy the id from of the chat object, ex: ... , "chat": {"id": 123456789, "first_name": ...
+"""
+#chat_id = -1001481984580
+#token = '819065046:AAFee77GqSpq8XBzmEnAMybLqOHuy6PJ_bg'
 
-def send_message(message):
-    HERA_BOT.sendMessage(chat_id=chat_id, text=message)
+# stores chat_id and tokens
+accounts = {
+    'default': (-1001481984580, '819065046:AAFee77GqSpq8XBzmEnAMybLqOHuy6PJ_bg'),
+    'parro': (125016709, '716431813:AAHaKh7gsBrMoexVs1Lm7gcfHct-3Y3WT4U'),
+    # <insert your chat_id and token here>
+}
+
+# caches created bots per account
+bots = { account:None for account in accounts.keys() }
+
+def get_bot(account):
+    """ Get or create a new bot and cache it in the dictionary.
+        Return bot and chat_id
+    """
+    if account not in accounts:
+        print('Invalid telegram bot account!')
+        return None, None
+    
+    chat_id, token = accounts[account]
+    if bots[account] is None:
+        bots[account] = telepot.Bot(token)
+
+    return bots[account], chat_id
+
+def send_message(message, account='default'):
+    bot, chat_id = get_bot(account)
+    bot.sendMessage(chat_id=chat_id, text=message)
 
 
 class TelegramBotKerasCallback(keras.callbacks.Callback):
@@ -41,6 +74,4 @@ class TelegramBotKerasCallback(keras.callbacks.Callback):
     
     # def on_train_end(self, logs={}):
     #     pass
-    
-
     
