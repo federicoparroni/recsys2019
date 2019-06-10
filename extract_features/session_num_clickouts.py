b395ef2938c05b4f64b0afe5b2979c9033a70dca
teomores <mattemoresc@gmail.com>
2019-06-10 11:41:10 +0200

    Add percentage to SessionNumClickouts and SessionNumInterItemImage

--- extract_features/session_num_clickouts.py
+++ extract_features/session_num_clickouts.py
@@ -9,9 +9,10 @@ from preprocess_utils.last_clickout_indices import expand_impressions
 class SessionNumClickouts(FeatureBase):
 
     """
-    This feature says for each session the number of clickouts
+    This feature says for each session the number of clickouts, both flat
+    and percentage
 
-    user_id | session_id | item_id | num_clickouts
+    user_id | session_id | item_id | num_clickouts | perc_clickouts
 
     """
 
@@ -31,13 +32,27 @@ class SessionNumClickouts(FeatureBase):
             .size()
             .reset_index(name='num_clickouts')
         )
-
+        # compute session length
+        sess_size = (
+            df.groupby(['user_id','session_id'])
+            .size()
+            .reset_index(name='session_length')
+        )
+        # get clk rows and expand
         clickout_rows = df.loc[find(df), ['user_id','session_id','action_type','impressions']][df.action_type == 'clickout item']
         clk_expanded = expand_impressions(clickout_rows).drop(['index','action_type'],1)
-
+        # merge
         final_feature = pd.merge(clk_expanded, feature, how='left', on=['user_id','session_id']).fillna(0)
         final_feature.num_clickouts = final_feature.num_clickouts.astype(int)
-        return final_feature
+        final_feature = pd.merge(final_feature, sess_size, how='left', on=['user_id','session_id']).fillna(0)
+        final_feature.session_length = final_feature.session_length.astype(int)
+        # compute the percentage
+        perc = []
+        for t in tqdm(zip(final_feature.num_clickouts, final_feature.session_length)):
+            perc.append((t[0]*100)/t[1])
+        final_feature['perc_clickouts'] = perc
+
+        return final_feature[['user_id','session_id','item_id','num_clickouts','perc_clickouts']]
 
 if __name__ == '__main__':
     import utils.menu as menu
--- extract_features/session_num_inter_item_image.py
+++ extract_features/session_num_inter_item_image.py
@@ -9,9 +9,10 @@ from preprocess_utils.last_clickout_indices import expand_impressions
 class SessionNumInterItemImage(FeatureBase):
 
     """
-    This feature says for each session the number of interaction with images
+    This feature says for each session the number of interaction with images,
+    both flat and percentage
 
-    user_id | session_id | item_id | num_inter_item_image
+    user_id | session_id | item_id | num_inter_item_image | perc_inter_item_image
 
     """
 
@@ -24,20 +25,34 @@ class SessionNumInterItemImage(FeatureBase):
         train = data.train_df(mode=self.mode, cluster=self.cluster)
         test = data.test_df(mode=self.mode, cluster=self.cluster)
         df = pd.concat([train, test])
-
+        # compute number of interactions per session
         df_int = df[df.action_type=='interaction item image'][['user_id','session_id','timestamp','step','action_type']]
         feature = (
             df_int.groupby(['user_id','session_id'])
             .size()
             .reset_index(name='num_inter_item_image')
         )
-
+        # compute session length
+        sess_size = (
+            df.groupby(['user_id','session_id'])
+            .size()
+            .reset_index(name='session_length')
+        )
+        # get clk rows and expand
         clickout_rows = df.loc[find(df), ['user_id','session_id','action_type','impressions']][df.action_type == 'clickout item']
         clk_expanded = expand_impressions(clickout_rows).drop(['index','action_type'],1)
-
+        # merge
         final_feature = pd.merge(clk_expanded, feature, how='left', on=['user_id','session_id']).fillna(0)
         final_feature.num_inter_item_image = final_feature.num_inter_item_image.astype(int)
-        return final_feature
+        final_feature = pd.merge(final_feature, sess_size, how='left', on=['user_id','session_id']).fillna(0)
+        final_feature.session_length = final_feature.session_length.astype(int)
+        # compute the percentage
+        perc = []
+        for t in tqdm(zip(final_feature.num_inter_item_image, final_feature.session_length)):
+            perc.append((t[0]*100)/t[1])
+        final_feature['perc_inter_item_image'] = perc
+
+        return final_feature[['user_id','session_id','item_id','num_inter_item_image','perc_inter_item_image']]
 
 if __name__ == '__main__':
     import utils.menu as menu
