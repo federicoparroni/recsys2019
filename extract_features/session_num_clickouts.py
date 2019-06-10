f8732e8fb6092c96c7ab1cf4336e9e95c60ac53b
teomores <mattemoresc@gmail.com>
2019-06-10 10:30:37 +0200

    NEW FEATURE: SessionNumClickouts

---
+++ extract_features/session_num_clickouts.py
@@ -0,0 +1,51 @@
+from extract_features.feature_base import FeatureBase
+import data
+import pandas as pd
+from tqdm.auto import tqdm
+import numpy as np
+from preprocess_utils.last_clickout_indices import find
+from preprocess_utils.last_clickout_indices import expand_impressions
+
+from collections import Counter
+
+class SessionNumClickouts(FeatureBase):
+
+    """
+    This feature does the same as PercClickPerImpressions, but computes the
+    popularity on the whole df (like PersonalizedTopPop)
+
+    user_id | session_id | item_id | adj_perc_click_appeared
+
+    """
+
+    def __init__(self, mode, cluster='no_cluster'):
+        name = 'session_num_clickouts'
+        super(SessionNumClickouts, self).__init__(
+            name=name, mode=mode, cluster=cluster)
+
+    def extract_feature(self):
+        train = data.train_df(mode=self.mode, cluster=self.cluster)
+        test = data.test_df(mode=self.mode, cluster=self.cluster)
+        df = pd.concat([train, test])
+
+        df_clk = df[df.action_type=='clickout item'][['user_id','session_id','timestamp','step','action_type']]
+        feature = (
+            df_clk.groupby(['user_id','session_id'])
+            .size()
+            .reset_index(name='num_clickouts')
+        )
+
+        return feature
+
+if __name__ == '__main__':
+    import utils.menu as menu
+
+    mode = menu.mode_selection()
+    cluster = menu.cluster_selection()
+
+    c = SessionNumClickouts(mode, cluster)
+
+    print('Creating {} for {} {}'.format(c.name, c.mode, c.cluster))
+    c.save_feature()
+
+    print(c.read_feature())
