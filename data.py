dd54a86274b0882fbfdea467650c6657755400d9
Daniele <danmontesi@gmail.com>
2019-06-14 00:31:53 +0200

    add dataset catboost to data

--- data.py
+++ data.py
@@ -39,6 +39,9 @@ _target_indices = {}
 _df_classification_train = {}
 _df_classification_test = {}
 
+_df_catboost_train = {}
+_df_catboost_test = {}
+
 _dataset_xgboost_train = {}
 _dataset_xgboost_test = {}
 
@@ -239,27 +242,24 @@ def classification_test_df(mode, sparse=True, cluster='no_cluster', algo='xgboos
     return _df_classification_test[tot_path]
 
 
-def user_prop_df(mode, sparse=True, cluster='no_cluster'):
-    global _user_prop
-    path = 'dataset/preprocessed/{}/{}/user_properties.csv'.format(cluster, mode)
 
-    if sparse:
-        tot_path = path + 'dense'
-    else:
-        tot_path = path + 'sparse'
+def dataset_catboost_train(mode, cluster='no_cluster'):
+    global _df_catboost_train
+    path = 'dataset/preprocessed/{}/{}/{}/train.csv'.format(cluster, mode, 'catboost')
 
-    if tot_path not in _user_prop:
-        if sparse:
-            data = ddf.read_csv(path)
-            data = data.map_partitions(lambda part: part.to_sparse(fill_value=0))
-            data = data.compute().reset_index(drop=True)
-            data = data.drop(['Unnamed: 0'], axis=1)
-            _user_prop[tot_path] = data
-        else:
-            _user_prop[tot_path] = pd.read_csv(path, index_col=0)
+    if path not in _df_catboost_train:
+        _df_catboost_train[path] = pd.read_csv(path)
 
-    return _user_prop[tot_path]
+    return _df_catboost_train[path]
 
+def dataset_catboost_test(mode, cluster='no_cluster'):
+    global _df_catboost_test
+    path = 'dataset/preprocessed/{}/{}/{}/test.csv'.format(cluster, mode, 'catboost')
+
+    if path not in _df_catboost_test:
+        _df_catboost_test[path] = pd.read_csv(path)
+
+    return _df_catboost_test[path]
 
 def train_indices(mode):
     global _full_train_indices
