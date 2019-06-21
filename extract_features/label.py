9cd95bbf37a7913f82cba94f71f38e8bd1f425c4
giovanni <giovanni.gabbolini@gmail.com>
2019-06-21 17:51:16 +0200

    fix in label

--- extract_features/label.py
+++ extract_features/label.py
@@ -43,10 +43,6 @@ class ImpressionLabel(FeatureBase):
         df = expand_impressions(df)
         df['label'] = (df['item_id'] == df['reference'].astype('float'))*1
         df.drop(['index', 'reference'], axis=1, inplace=True)
-        print('len of df')
-        print(len(df))
-        print('len of groups')
-        print(len(df.groupby(['user_id', 'session_id', 'item_id'])))
 
         print(df)
         return df
